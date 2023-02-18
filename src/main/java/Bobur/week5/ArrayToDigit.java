1f0e38714e85fc20eb83ba38cb6d8bcd320787e2
Rikhsiboev <rikhsiboev@gmail.com>
2023-02-17 19:27:34 -0700

    ArrayToDigit

---
+++ src/main/java/Bobur/week5/ArrayToDigit.java
@@ -0,0 +1,11 @@
+package Bobur.week5;
+
+import java.util.Arrays;
+
+public class ArrayToDigit {
+    public static void main(String[] args) {
+        String str = "490";
+        System.out.println(Arrays.asList(str.split("")));
+
+    }
+}

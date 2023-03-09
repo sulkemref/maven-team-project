4a242de18f42b073334095e67f648bfaa5e32216
Kemail-terminal <sulkemref@gmail.com>
2023-03-09 12:49:41 -0600

    Week 09

---
+++ src/main/java/kemal/PassValue.java
@@ -0,0 +1,17 @@
+package kemal;
+
+public class PassValue {
+
+    public static void main(String[] args) {
+
+        Integer a = 10;
+
+        addInteger(a);
+
+        System.out.println(a);
+
+    }
+    public static void addInteger(Integer a){
+
+    }
+}

906c24e1fdac2efc59dbbaac8d5725b844d49d4a
Almi's PC <almimeci@gmail.com>
2023-05-03 10:19:44 -0500

    Week 16 : Q1

--- src/main/java/almi/JavaCodingTasks/week16/IntersectionOfList.java
+++ src/main/java/almi/JavaCodingTasks/week16/IntersectionOfList.java
@@ -9,19 +9,21 @@ public class IntersectionOfList {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
         Set<ListNode> listNodes = new HashSet<>();
+        // create Set because we will be looking for intersection by NodeAddress NOT NodeValues
 
         ListNode currentA = headA;
         ListNode currentB = headB;
+        // specifying current node(heads)
 
         while (currentA!=null){
-           listNodes.add(currentA);
-           currentA = currentA.next;
+           listNodes.add(currentA);             // add each element of A to our Set
+           currentA = currentA.next;            // iterate after each elem
         }
 
         while (currentB != null) {
-            if (listNodes.contains(currentB)){
+            if (listNodes.contains(currentB)){ // if currentB(head) {byAddress} is in our Set
                 return currentB;
-            } currentB = currentB.next;
+            } currentB = currentB.next;        // iterate to next node
         }
 
         return null;

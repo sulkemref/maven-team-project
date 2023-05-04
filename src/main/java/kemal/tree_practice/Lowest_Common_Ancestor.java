b0129b4087787d37cd95d071514bab310e7c7581
Kemail-terminal <sulkemref@gmail.com>
2023-05-04 15:41:40 -0500

    lowestCommonAncestorWhile done

--- src/main/java/kemal/tree_practice/Lowest_Common_Ancestor.java
+++ src/main/java/kemal/tree_practice/Lowest_Common_Ancestor.java
@@ -6,12 +6,24 @@ public class Lowest_Common_Ancestor {
 
 //    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    public TreeNode lowestCommonAncestorRec(TreeNode root, TreeNode p, TreeNode q) {
         if(q.val<root.val&&root.val<p.val ||p.val<root.val&&root.val<q.val // case 1
                 ||p==root  // case 2
                 ||q==root) // case 3
             return root;
-        return lowestCommonAncestor(q.val>root.val&&p.val>root.val?root.right:root.left,p,q);
+        return lowestCommonAncestorRec(q.val>root.val&&p.val>root.val?root.right:root.left,p,q);
         // if p & q > root move to right else to left
+    }
+
+    public TreeNode lowestCommonAncestorWhile(TreeNode root, TreeNode p, TreeNode q){
+        while (root!=null){
+            if(q.val<root.val&&root.val<p.val ||p.val<root.val&&root.val<q.val // case 1
+                    ||p==root  // case 2
+                    ||q==root) // case 3
+                break;
+            else
+                root=q.val>root.val&&p.val>root.val?root.right:root.left;
+        }
+        return root;
     }
 }
